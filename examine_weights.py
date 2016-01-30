a7b43d41b7ee40617344aeb42f1b30dd3e1ec1b3
Howon Lee <howon@howonlee.com>
2016-01-29 21:27:40 -0800

    beautiful comparison to the init random stuff

--- examine_weights.py
+++ examine_weights.py
@@ -7,15 +7,20 @@ from mlp import create_mnist_samples, onehots
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    FILENAME = "weight_mat.npy"
-    network_mat = np.abs(np.load(FILENAME))
+    INIT_FILENAME = "init_mat.npy"
+    NET_FILENAME = "weight_mat.npy"
+    network_mat = np.load(NET_FILENAME)
+    init_mat = np.load(INIT_FILENAME)
     max_network_member = np.max(network_mat)
     network_mat /= max_network_member
     # samples, _ = create_mnist_samples()
     # print np.mean(samples['input'][:30000][:, 0])
     print network_mat.shape
-    plt.hist(network_mat.ravel())
-    plt.show()
+    for x in xrange(30):
+        plt.close()
+        plt.plot(network_mat[x, :])
+        plt.plot(init_mat[x, :], "r")
+        plt.show()
     # for x in xrange(30):
     #     plt.close()
     #     plt.hist(network_mat[:, x], bins=30)
